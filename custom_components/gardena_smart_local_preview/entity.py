cb283aa84cd9c5993deba37dbd3e41d09ea49267
Reto Schneider <reto.schneider@husqvarnagroup.com>
2026-05-18 14:48:34 +0200

    Achieve REUSE compliance
    By adding license and copyright information to all files in the
    repository we make it much easier for downstream to use this library in
    a compliant way.

--- custom_components/gardena_smart_local_preview/entity.py
+++ custom_components/gardena_smart_local_preview/entity.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2026 GARDENA GmbH
+#
+# SPDX-License-Identifier: Apache-2.0
+
 from __future__ import annotations
 
 from homeassistant.helpers import device_registry as dr
